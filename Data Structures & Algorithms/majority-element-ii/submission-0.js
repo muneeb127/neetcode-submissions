5bbcbb6349e3840e66007212dd9ba4283e2d5ef8
Muneeb Ahmed Khan <muneebahmed_127@hotmail.com>
2026-05-17 13:13:41 +0500

    Add: majority-element-ii - submission-0

---
+++ Data Structures & Algorithms/majority-element-ii/submission-0.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    majorityElement(nums) {
+        let map = new Map();
+
+        let result = [];
+        let n = nums.length;
+
+        for(let num of nums){
+            map.set(num, (map.get(num) || 0) + 1);
+            if(map.get(num) > n/3 && !result.includes(num)) result.push(num);
+        }
+
+        return result;
+    }
+}
