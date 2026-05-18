5229cf4a4082ae5343214edaa6094b7dbce1d485
Muneeb Ahmed Khan <muneebahmed_127@hotmail.com>
2026-05-18 23:27:04 +0500

    Add: reverse-string - submission-0

---
+++ Data Structures & Algorithms/reverse-string/submission-0.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {character[]} s
+     * @return {void} Do not return anything, modify s in-place instead.
+     */
+    reverseString(s) {
+        // let str = s.join('');
+        // console.log(str);
+
+        // let arr = str.split('').
+        // let left = 0; 
+        // let right = s.length - 1;
+
+        // while(left < right){
+        //     [s[left], s[right]] = [s[right], s[left]];
+
+        //     left++;
+        //     right--;
+        // }
+
+        return s.reverse();
+    }
+}
