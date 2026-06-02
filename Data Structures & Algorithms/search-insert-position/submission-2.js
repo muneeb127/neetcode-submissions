f48f4d22a8ba7e69374be393ef7bad44e8432588
Muneeb Ahmed Khan <muneebahmed_127@hotmail.com>
2026-06-02 11:05:12 +0500

    Add: search-insert-position - submission-2

---
+++ Data Structures & Algorithms/search-insert-position/submission-2.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    searchInsert(nums, target) {
+        let low = 0; 
+        let high = nums.length - 1;
+
+        while(low <= high){
+            let mid = low + Math.floor((high - low) / 2);
+
+            if(nums[mid] === target) return mid;
+            else if (nums[mid] < target) low = mid + 1;
+            else high = mid - 1;
+        }
+
+        return low;
+    }
+}
