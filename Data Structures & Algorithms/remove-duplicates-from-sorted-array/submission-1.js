1676395c8681436766aef5bc777c9979169281d8
Muneeb Ahmed Khan <muneebahmed_127@hotmail.com>
2026-06-02 13:44:13 +0500

    Add: remove-duplicates-from-sorted-array - submission-1

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-1.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    removeDuplicates(nums) {
+        let k = 1;
+
+        for(let i = 1; i < nums.length; i++){
+            if(nums[k - 1] !== nums[i]){
+                nums[k++] = nums[i]
+            }
+        }
+
+        return k;
+    }
+}
